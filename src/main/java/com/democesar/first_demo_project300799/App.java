31203f30c963c84b21120a789822fa7ce4b8675d
Cesar Rosales <brian_vinn@hotmail.com>
2023-04-19 10:03:30 -0600

    added initial commit

---
+++ src/main/java/com/democesar/first_demo_project300799/App.java
@@ -0,0 +1,13 @@
+package com.democesar.first_demo_project300799;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
